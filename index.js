19494640696fb7d43842369f6ef7778966ece9ca
Christian Khan <macpro@Christians-MacBook-Pro.local>
2020-09-24 12:50:17 -0400

    Update MongoDB Driver to include promises

--- index.js
+++ index.js
@@ -8,34 +8,39 @@ const dbname = 'conFusion';
  * The Node MongoDB Driver
  */
 //Access Server
-MongoClient.connect(url, (err, client) => {
-    assert.strictEqual(err, null);
+MongoClient.connect(url).then((client) => {
+    
 
     console.log('Connected correctly to server');
     //connect to DB
     const db = client.db(dbname);
 
-    dboper.insertDocument(db, { name: "Vadonut", description: 'Test' }, 'dishes', (result) => {
-        //ops tells you how many operations have been carried out
-        console.log('Insert Document:\n', result.ops);
+    dboper.insertDocument(db, { name: "Vadonut", description: 'Test' },'dishes')
+        .then((result) => {
+            console.log('Insert Document:\n', result.ops);
 
-        dboper.findDocuments(db, 'dishes', (docs) => {
+            return dboper.findDocuments(db, 'dishes')
+        })
+        .then((docs) => {
             console.log('Found Documents:\n', docs);
 
-            dboper.updateDocument(db, { name: "Vadonut"}, {description: 'Updated Test' }, 'dishes', (result) => {
-                console.log('Updated Document:\n', result.result);
+            return dboper.updateDocument(db, { name: "Vadonut" }, { description: 'Updated Test' }, 'dishes')
+        })
+        .then((result) => {
+            console.log('Updated Document:\n', result.result);
 
-                dboper.findDocuments(db, 'dishes', (docs) => {
-                    console.log('Found Documents:\n', docs);
+            return dboper.findDocuments(db, 'dishes')
+        })
+        .then((docs) => {
+            console.log('Found Documents:\n', docs);
 
-                    db.dropCollection('dishes', (result) => {
-                        console.log('Dropped Collection:\n ', result);
+            return db.dropCollection('dishes')
+        })
+        .then((result) => {
+            console.log('Dropped Collection:\n ', result);
 
-                        client.close();
-                    });
-                });
-            });
-
-        });
-    });
-});
+            return client.close();
+        })
+        .catch((err) => console.log(err)); 
+})
+.catch((err) => console.log(err)); 
